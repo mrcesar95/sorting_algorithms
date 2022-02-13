195c909d3045d000c6efeee2fc44560e71b20a91
Cesar Molina <cesaruxfxtx@gmail.com>
2022-02-13 13:54:19 -0500

    Fixing up efficiency tests

--- sort.h
+++ sort.h
@@ -19,17 +19,13 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** Given FUN prototype */
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
-/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 #endif
