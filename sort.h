4a5cbd3df1456949161268c5bffdce29a338d001
Cesar Molina <cesaruxfxtx@gmail.com>
2022-02-13 11:02:12 -0500

    Fixing up sort.h

--- sort.h
+++ sort.h
@@ -6,16 +6,16 @@
 
 /**
  * struct listint_s - Doubly linked list node
- * 
+ *
  * @n: Integer stored in the node
- * @prev: Poniter to the previous element of the list
+ * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
 typedef struct listint_s
 {
-		const int n;
-		struct listint_s *prev;
-		struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
 void print_array(const int *array, size_t size);
@@ -26,5 +26,3 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 
 #endif
-
-
