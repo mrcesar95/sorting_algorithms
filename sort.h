5647f550dc30454000118b7b4da1f1760320c15b
Cesar Molina <cesaruxfxtx@gmail.com>
2022-02-13 11:40:59 -0500

    Fixing up files

--- sort.h
+++ sort.h
@@ -1,5 +1,5 @@
-#ifndef SORT_H
-#define SORT_H
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
 #include <stdlib.h>
 #include <string.h>
@@ -26,5 +26,6 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
 
 #endif
