48996071707b670b9a1d16743aee4ddbce8ac24c
Cesar Molina <cesaruxfxtx@gmail.com>
2022-02-13 11:16:03 -0500

    Fixing up sort.h

--- sort.h
+++ sort.h
@@ -1,9 +1,9 @@
-#ifndef _SORTING_H_
-#define _SORTING_H_
+#ifndef HEADER_FILE
+#define HEADER_FILE
+
+#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
-
-
-/** Struct Double Linked List */
 
 /**
  * struct listint_s - Doubly linked list node
@@ -19,17 +19,12 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/** Given FUN prototype */
-
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
-
-/** TASK Prototype */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
 
 #endif
